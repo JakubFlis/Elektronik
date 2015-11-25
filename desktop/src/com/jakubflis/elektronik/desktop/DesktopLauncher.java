71bbdd781754213fb1ee9af3f4a2042654d63a46
Jakub Flis <J.Flis@kainos.com>
2015-11-25 11:49:45 +0100

    Init commit

---
+++ desktop/src/com/jakubflis/elektronik/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jakubflis.elektronik.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jakubflis.elektronik.JFElektronik;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JFElektronik(), config);
+	}
+}
